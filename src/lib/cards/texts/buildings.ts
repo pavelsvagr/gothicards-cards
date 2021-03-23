b1dda7184b8dd516c1a18b2e79dfb32123633f6a
Pavel Svagr <svagrpa1@fit.cvut.cz>
2021-03-23 23:23:07 +0100

    🐛 Fix old cave definition

--- src/lib/cards/texts/buildings.ts
+++ src/lib/cards/texts/buildings.ts
@@ -3,6 +3,8 @@ import { Quote } from '../../formatting'
 const descriptions: { [slug: string]: Quote | string } = {
   // A
   abandonedMine: 'Once upon a time, there was a life. Now the only thing you can find here is death and some not mined ore.',
+  // O
+  oldCave: 'Place that only fool can visit alone.',
 }
 
 export default descriptions
