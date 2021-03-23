6a392540aa611f408115b056ee45ba855e7ebcae
pavelsvagr <47928947+pavelsvagr@users.noreply.github.com>
2021-03-24 00:05:25 +0100

    ♻️ Removed useless comment

--- src/lib/cardLoader.ts
+++ src/lib/cardLoader.ts
@@ -7,9 +7,6 @@ const cardsBySlug = new Map<string, CardDefinition>(
   allCards.map(card => [card.slug, card])
 )
 
-/**
- * Loades card by given slug
- */
 export const loadCardBySlug = (slug: string): CardDefinition => {
   const cardDef = cardsBySlug.get(slug)
   if (cardDef) {
