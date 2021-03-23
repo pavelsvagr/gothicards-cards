c6a93fced3c9852964f74832a9d676a1cd843d74
Pavel Svagr <svagrpa1@fit.cvut.cz>
2021-03-23 23:12:03 +0100

    🔧 Prepare missing configuration files for linting and compiling.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,7 @@
-module.exports = require('@ackee/styleguide-backend-config/eslint')
+module.exports = {
+  ...require('@ackee/styleguide-backend-config/eslint'),
+  ignorePatterns: ['dist'],
+  parserOptions: {
+    project: '.eslint.tsconfig.json',
+  },
+}
