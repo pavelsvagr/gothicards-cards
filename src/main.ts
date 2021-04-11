8e3bdf1837ff60cc4e4e64177ba785952c002bb6
Pavel Svagr <svagrpa1@fit.cvut.cz>
2021-04-11 18:23:35 +0200

    ✨ Add more exports, use declaration

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,42 @@ export * as effects from './lib/cards/effects'
 export * as buildings from './lib/cards/buildings'
 
 export { loadCardBySlug } from 'lib/cardLoader'
+export {
+  CardDefinition,
+  WeaponProps,
+  ArmorProps,
+  CardEffects,
+  BuildingCardProps,
+  UnitCardProps,
+} from 'lib/definitions'
+export {
+  Guilds,
+  StatusTypes,
+  DamageTypes,
+  CardRarity,
+  Fractions,
+  Sides,
+  Signs,
+  AttackTypes,
+  BaseTargets,
+  EffectTypes,
+  Collections,
+  ItemTypes,
+  CardType,
+  Comparisons,
+  DeathTypes,
+} from 'lib/enums'
+
+export { ConditionDefinition, ConditionHasDefinition } from 'lib/conditions'
+export { Status } from 'lib/statuses'
+export {
+  EFFECT_SELECTION,
+  EFFECT_COMBINATION,
+  COUNT_ALL,
+  EffectDefinition,
+  TargetDefinition,
+  ValueDefinition,
+  AbilityDefinition,
+  AttackDefinition,
+  RandomDefinition,
+} from 'lib/effects'
